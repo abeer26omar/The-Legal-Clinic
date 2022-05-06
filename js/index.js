4185f9808efaba6117aace028917b8dd56707fc3
abeer26omar <abeer.omar504@gmail.com>
2022-05-06 20:41:25 +0200

    finish about and index

--- js/index.js
+++ js/index.js
@@ -235,3 +235,12 @@ $(window).on("scroll", function() {
         }
     }
 });
+// button toggle
+$('.switch label').on('click', function(){
+  var indicator = $(this).parent('.switch').find('span');
+  if ( $(this).hasClass('right') ){
+		$(indicator).addClass('right');
+  } else {
+    $(indicator).removeClass('right');
+  }
+});
